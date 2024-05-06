6da2c989dcf6a0efc4c9240edc9beb2023f4e57a
SitaGomes <ansitagomes@gmail.com>
2024-05-06 17:57:47 -0300

    feat: Adicionar mensagem de oferta na seção de crédito na página inicial

--- src/views/app/Home/index.tsx
+++ src/views/app/Home/index.tsx
@@ -110,18 +110,18 @@ const HomePage: React.FC = () => {
 
       {/* CREDIT */}
       <section className="text-white rounded-2xl flex flex-wrap justify-center items-center p-4 gap-5 lg:justify-between border-2 border-yellow-400">
+        <div className="w-full text-center">
+          Por apenas R$ 9,90 você tem acesso ilimitado a plataforma por 30 dias,
+          boas apostas!
+        </div>
         <div className="flex justify-between items-center w-full gap-4">
           <a
             target="_blank"
-            href="https://buy.stripe.com/7sIbMl6Jx8T28Rq9Bo"
+            href="https://buy.stripe.com/aEUg2B0l9d9iffObJp"
             className="bg-green-500 p-2 rounded-md text-white text-center w-full"
           >
             Comprar
           </a>
-        </div>
-        <div className="w-full text-center">
-          Por apenas R$ 9,90 você tem acesso ilimitado a plataforma por 30 dias,
-          basta clicar em comprar, boas apostas!
         </div>
       </section>
 
